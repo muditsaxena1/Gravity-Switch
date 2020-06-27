4b43f76d9b660f594e2f9585a1b984284a905c48
muditsaxena1 <mudit.25102010@gmail.com>
2020-06-28 00:58:17 +0530

    added pause menu

---
+++ Assets/_Scripts/PauseController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseController : MonoBehaviour
+{
+    public GameObject pauseMenu;
+
+    public void OnPause()
+    {
+        Debug.Log("Pausing Game");
+        Time.timeScale = 0f;
+        pauseMenu.SetActive(true);
+    }
+
+    public void OnResume()
+    {
+        Debug.Log("Resuming Game");
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void MainMenuClick()
+    {
+        Debug.Log("Loading main menu");
+    }
+
+    public void RestartClick()
+    {
+        Debug.Log("Restarting the lvl");
+    }
+}
