b1640302ff9bed51d4d200647c51109a5c1a47d9
NishBrod <43043984+NishBrod@users.noreply.github.com>
2020-06-30 04:35:56 -0400

    levelPage

--- Assets/Scenes/Level Page/LevelPage.cs
+++ Assets/Scenes/Level Page/LevelPage.cs
@@ -26,7 +26,8 @@ public class LevelPage : MonoBehaviour
     [System.Serializable]
     public class ShopItem
     {
-        public string SceneName;
+        //public string SceneName;
+        public bool isUnlocked=false;
     }
     public List<ShopItem> ShopItemsList;
     
@@ -38,24 +39,42 @@ public class LevelPage : MonoBehaviour
     [SerializeField] GameObject ShopPanel;
     Button buyBtn;
     Text levelNumber;
-    
-    
 
+    public Color levelColor1;
+    public Color levelColor2;
+     Image lockImage;
 
 
     void Start()
-    {
-
-        
-
+    { 
         int len = ShopItemsList.Count;
         for (int i = 0; i < len; i++)
         {
             eachItem = Instantiate(ItemTemplate, ShopScrollView);
           
             buyBtn = eachItem.transform.GetChild(0).GetComponent<Button>();
+            lockImage = eachItem.transform.GetChild(0).GetChild(1).GetComponent<Image>();
             levelNumber = buyBtn.GetComponentInChildren<Text>();
-            levelNumber.text = (i+1).ToString();
+            levelNumber.text = (i + 1).ToString();
+            /**if (ShopItemsList[i].isUnlocked)
+            {
+                lockImage.enabled = false;
+                
+                levelNumber.enabled = true;
+               // levelNumber.text = (i + 1).ToString();
+
+            }
+            else
+            {
+                lockImage.enabled = true;
+
+                levelNumber.enabled = false;
+
+            }**/
+            /**levelNumber = buyBtn.GetComponentInChildren<Text>();
+            levelNumber.text = (i+1).ToString();**/
+            
+            levelColors(i);
 
 
            
@@ -74,6 +93,34 @@ public class LevelPage : MonoBehaviour
         SceneManager.LoadScene((itemIndex + 1).ToString());
     }
 
-   
+   void levelColors(int i)
+    {
+        if (i > 9 && i < 20)
+        {
+            buyBtn.GetComponent<Image>().color = levelColor1;
+        }
+        else if (i > 19)
+        {
+            buyBtn.GetComponent<Image>().color = levelColor2;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        for (int i = 0; i < ShopItemsList.Count; i++)
+        {
+            if (ShopItemsList[i].isUnlocked)
+            {
+                lockImage.enabled = false;
+                levelNumber.enabled = true;
+            }
+            else
+            {
+                lockImage.enabled = true;
+                levelNumber.enabled = false;
+
+            }
+        }
+    }
 
 }
