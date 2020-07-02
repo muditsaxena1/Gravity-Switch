839df38726dcad951518d2034af970b01871fe75
muditsaxena1 <mudit.25102010@gmail.com>
2020-07-02 15:40:57 +0530

    Load n Save

---
+++ Assets/_Scripts/DataFormat.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class DataFormat
+{
+    public int illuminatiCount;
+    public int diamondCount;
+    public int[] levelStars;        //-1 means not unlocked 0,1,2,3 tells number of stars
+    public bool[] skinsUnlocked;
+    public int currentSkin;
+
+    public DataFormat(LoadSaveManager loadSaveManager)
+    {
+        illuminatiCount = loadSaveManager.IlluminatiCount;
+        diamondCount = loadSaveManager.DiamondCount;
+        levelStars = loadSaveManager.LevelStars;
+        skinsUnlocked = loadSaveManager.SkinsUnlocked;
+        currentSkin = loadSaveManager.CurrentSkin;
+    }
+
+}
---
+++ Assets/_Scripts/SaveSystem.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public static class SaveSystem
+{
+    static string path = Application.persistentDataPath + "/GarvitySwitchData.dat";
+
+    public static void SaveData(LoadSaveManager loadSaveManager)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        DataFormat data = new DataFormat(loadSaveManager);
+
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+
+    public static DataFormat LoadData()
+    {
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            DataFormat data = formatter.Deserialize(stream) as DataFormat;
+            stream.Close();
+            return data;
+        }
+        else
+        {
+            Debug.LogWarning("Data file doesn't exists in " + path);
+            return null;
+        }
+    }
+}
